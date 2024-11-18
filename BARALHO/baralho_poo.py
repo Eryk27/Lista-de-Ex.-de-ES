5d5214d640516db84db7a589239ee7d7a74e57e6
Eryk27 <eryk.oliveira@academico.ifpb.edu.br>
2024-11-18 17:52:07 -0300

    baralho

---
+++ BARALHO/baralho_poo.py
@@ -0,0 +1,18 @@
+class Carta:
+    def __init__(self, valor, naipe):
+        self.valor = valor
+        self.naipe = naipe
+class Baralho:
+    def __init__(self):
+        self.cartas = []
+
+    def misturar(self):
+
+    def distribuir_cartas(self, jogadores): 
+class Jogador:
+    def __init__(self, nome):
+        self.nome = nome
+        self.mao = []
+
+    def adicionar_cartas(self, cartas):
+    
